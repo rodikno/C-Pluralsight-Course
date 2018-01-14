dae05bd50abe89b67d89389ed598da4ce60331d8
Rodion Baronov <rodikno@gmail.com>
2018-01-14 12:13:27 +0200

    Added type tests

--- CSharp_Pluralsight/Grades.Tests/TypeTests.cs
+++ CSharp_Pluralsight/Grades.Tests/TypeTests.cs
@@ -12,6 +12,22 @@ namespace Grades.Tests
     public class TypeTests
     {
         [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
         public void ValueTypesPassByValue()
         {
             int x = 46;
